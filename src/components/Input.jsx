0bd51121ec55bf5548bfd5f8b45aec5d6c2d5247
Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>
2024-04-09 17:07:21 +0200

    feat: add basic data validation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,12 +2,7 @@ export const Input = ({ label, id, type, value, action }) => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        type={type}
-        value={value}
-        onChange={action}
-      />
+      <input id={id} type={type} value={value} onChange={action} required />
     </>
   );
 };
